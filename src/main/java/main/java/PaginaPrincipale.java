558fa5f34d55c6351f713564521382a0bab82dfe
Marco Nunnari <marnunna@gmail.com>
2015-05-27 13:14:57 +0200

    ancora centraggio

--- src/main/java/main/java/PaginaPrincipale.java
+++ src/main/java/main/java/PaginaPrincipale.java
@@ -65,7 +65,7 @@ public class PaginaPrincipale extends HttpServlet {
 				String telefono = contatto.getTelefono();
 				
 	       		out.append("			<tr>");
-	       		out.append("			<td> <input id=\"rig"+i+"\" name=\"sel\" type=\"radio\" value=\""+i+"\" /> </td>");
+	       		out.append("			<td> <input class=\"sel\" id=\"rig"+i+"\" name=\"sel\" type=\"radio\" value=\""+i+"\" /> </td>");
 	       		out.append("			<td>&nbsp" + nome + "&nbsp</td>");
 	       		out.append("			<td>&nbsp" + cognome + "&nbsp</td>");
 	       		out.append("			<td>&nbsp" + telefono + "&nbsp</td>");
